38db9c9daee33522d8108b066832a0984417f7f2
Bianca Muniz <41485608+biamuniz@users.noreply.github.com>
2021-09-28 14:05:20 -0300

    Update week39.R

--- week39/week39.R
+++ week39/week39.R
@@ -2,11 +2,10 @@ install.packages("tidytuesdayR")
 library(tidyverse)
 
 esquisse::esquisser()
-
-tuesdata <- tidytuesdayR::tt_load('2021-09-21')
 tuesdata <- tidytuesdayR::tt_load(2021, week = 39)
-
 nominees <- tuesdata$nominees
+write.csv2(nominees, "nominees.csv")
+
+got <- nominees %>% filter(title %in% c("Game Of Thrones"))
 
 
-winners <- nominees %>% filter(type %in% c("Winner"))
