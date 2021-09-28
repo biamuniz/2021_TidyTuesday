f3b9f9856b0b128ee4034f7039cc4c488cab3342
Bianca Muniz <41485608+biamuniz@users.noreply.github.com>
2021-09-28 14:25:46 -0300

    Update week39.R

--- week39/week39.R
+++ week39/week39.R
@@ -1,11 +1,27 @@
-install.packages("tidytuesdayR")
 library(tidyverse)
+library(tidytuesdayR)
 
-esquisse::esquisser()
+
 tuesdata <- tidytuesdayR::tt_load(2021, week = 39)
 nominees <- tuesdata$nominees
 write.csv2(nominees, "nominees.csv")
 
 got <- nominees %>% filter(title %in% c("Game Of Thrones"))
 
+glimpse(got)
+got$logo <- NULL
+got$production <- NULL
+got$page <- NULL
+got$page_id <- NULL
 
+got_clean <- got %>% 
+  arrange(year) %>% 
+  distinct(category, type, title, distributor, producer, year, .keep_all = TRUE)
+
+got_clean %>%
+ filter(type %in% "Winner") %>%
+ ggplot() +
+ aes(x = year, fill = type) +
+ geom_bar() +
+ scale_fill_hue() +
+ theme_minimal()
